71301cf61894e5575c4dd305ad48f622a63193e4
ajuarez05 <a.juarez.0529@gmail.com>
2026-06-07 20:23:05 -0500

    improve memories page image sizing

--- app/(tabs)/memories.tsx
+++ app/(tabs)/memories.tsx
@@ -1,4 +1,4 @@
-import { Image, ScrollView, Text } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
 
 export default function MemoriesScreen() {
   return (
@@ -9,12 +9,13 @@ export default function MemoriesScreen() {
       }}
       contentContainerStyle={{
         padding: 20,
+        paddingBottom: 40,
       }}
     >
       <Text
         style={{
           color: '#89CFF0',
-          fontSize: 30,
+          fontSize: 32,
           fontWeight: 'bold',
           marginBottom: 25,
         }}
@@ -22,66 +23,77 @@ export default function MemoriesScreen() {
         📸 Our Memories
       </Text>
 
-      <Image
-        source={require('../../assets/images/together.jpg')}
-        style={{
-          width: '100%',
-          height: 400,
-          borderRadius: 20,
-        }}
-      />
+      {/* Graduation */}
+      <View style={{ marginBottom: 35 }}>
+        <Image
+          source={require('../../assets/images/together.jpg')}
+          resizeMode="contain"
+          style={{
+            width: '100%',
+            height: 600,
+            borderRadius: 20,
+          }}
+        />
 
-      <Text
-        style={{
-          color: 'white',
-          fontSize: 18,
-          marginTop: 15,
-        }}
-      >
-        One of us is a graduate!
-      </Text>
+        <Text
+          style={{
+            color: 'white',
+            fontSize: 20,
+            marginTop: 15,
+          }}
+        >
+          One of us is a graduate! 🎓
+        </Text>
+      </View>
 
-      <Image
-        source={require('../../assets/images/nov.jpg')}
-        style={{
-          width: '100%',
-          height: 400,
-          borderRadius: 20,
-          marginTop: 30,
-        }}
-      />
+      {/* November */}
+      <View style={{ marginBottom: 35 }}>
+        <Image
+          source={require('../../assets/images/nov.jpg')}
+          resizeMode="contain"
+          style={{
+            width: '100%',
+            height: 600,
+            borderRadius: 20,
+          }}
+        />
 
-      <Text
-        style={{
-          color: 'white',
-          fontSize: 18,
-          marginTop: 15,
-        }}
-      >
-        The start of our journey together, as partners; not friends. I still
-        remember how your smile lit up the whole place.
-      </Text>
+        <Text
+          style={{
+            color: 'white',
+            fontSize: 20,
+            marginTop: 15,
+            lineHeight: 28,
+          }}
+        >
+          The start of our journey together, as partners and not just
+          friends. I still remember how your smile lit up the whole room.
+        </Text>
+      </View>
 
-      <Image
-        source={require('../../assets/images/dells.jpg')}
-        style={{
-          width: '100%',
-          height: 400,
-          borderRadius: 20,
-          marginTop: 30,
-        }}
-      />
+      {/* Wisconsin Dells */}
+      <View>
+        <Image
+          source={require('../../assets/images/dells.jpg')}
+          resizeMode="contain"
+          style={{
+            width: '100%',
+            height: 600,
+            borderRadius: 20,
+          }}
+        />
 
-      <Text
-        style={{
-          color: 'white',
-          fontSize: 18,
-          marginTop: 15,
-          marginBottom: 40,
-        }}
-      >
-        Still one of my favorite memories 💙
-      </Text>
+        <Text
+          style={{
+            color: 'white',
+            fontSize: 20,
+            marginTop: 15,
+            lineHeight: 28,
+          }}
+        >
+          Still one of my favorite memories 💙
+        </Text>
+      </View>
     </ScrollView>
   );
 }
